781f5a20a3414ef91cca72ede3803d368c53821a
Carl Hain <crhain@gmail.com>
2019-08-28 17:29:44 -0500

    section3: create a chart showing planet data as different sized cirlces of different colors

--- section3/index.js
+++ section3/index.js
@@ -1,22 +1,23 @@
-const data = [
-  { width: 200, height: 100, fill: 'purple' },
-  { width: 100, height: 60, fill: 'pink' },
-  { width: 50, height: 30, fill: 'red' }
-];
-
+// select svg container first
 const svg = d3.select('svg');
 
-//example of including data and deriving attributes from the data
-const rects = svg
-  //select multiple elements
-  .selectAll('rect')
-  //join data to rects
-  .data(data)
-  //enter part of cycle where virtual nodes can be updated
-  .enter()
-  //create a node for each data point
-  .append('rect')
-  //set attributes
-  .attr('width', d => d.width)
-  .attr('height', d => d.height)
-  .attr('fill', d => d.fill);
+//get data from json file asyncronously
+d3.json('planets.json').then(data => {
+  const circs = svg.selectAll('circle').data(data);
+
+  // add attrs to circs already in DOM
+  circs
+    .attr('cy', 200)
+    .attr('cx', d => d.distance)
+    .attr('r', d => d.radius)
+    .attr('fill', d => d.fill);
+
+  // append the enter selection to the DOM
+  circs
+    .enter()
+    .append('circle')
+    .attr('cy', 200)
+    .attr('cx', d => d.distance)
+    .attr('r', d => d.radius)
+    .attr('fill', d => d.fill);
+});
