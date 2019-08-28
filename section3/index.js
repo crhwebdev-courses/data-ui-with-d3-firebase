db28f91ae663a0843499cd01461d29171f8452a8
Carl Hain <crhain@gmail.com>
2019-08-28 16:53:35 -0500

    section3: select multiple rects within the svg and apply data from an array to each one

--- section3/index.js
+++ section3/index.js
@@ -1,11 +1,15 @@
-const data = [{ width: 200, height: 100, fill: 'purple' }];
+const data = [
+  { width: 200, height: 100, fill: 'purple' },
+  { width: 100, height: 60, fill: 'pink' },
+  { width: 50, height: 30, fill: 'red' }
+];
 
 const svg = d3.select('svg');
 
 //example of including data and deriving attributes from the data
 const rect = svg
-  //select dom element
-  .select('rect')
+  //select multiple element
+  .selectAll('rect')
   //include data
   .data(data)
   //set attributes
