0dfe674d2f41a920aad1fd357a41600a0fe25393
Carl Hain <crhain@gmail.com>
2019-08-31 15:24:02 -0500

    section4: draw first bar in graph that corresponds to first item in menu.json

--- section4/index.js
+++ section4/index.js
@@ -1,1 +1,14 @@
+//select html element where d3 will do its magic
 const svg = d3.select('svg');
+
+//get data from json file
+d3.json('menu.json').then(data => {
+  // join the data to rects
+  const rects = svg.selectAll('rect').data(data);
+
+  rects
+    .attr('width', 50)
+    .attr('height', d => d.orders)
+    .attr('fill', 'orange')
+    .attr('x', (d, i) => i * 70);
+});
