275a6cab99460c457d532c5f3fead691f6feebca
Carl Hain <crhain@gmail.com>
2019-08-31 15:58:23 -0500

    section4: set scale of bar chart using d3 scaleLinear function; reverse direction of bars by setting y coordinate with a formula that subtracts scaled value from y max scale value

--- section4/index.js
+++ section4/index.js
@@ -3,21 +3,31 @@ const svg = d3.select('svg');
 
 //get data from json file
 d3.json('menu.json').then(data => {
+  //create scale for y value -
+  //this will scale the values in the domain (data set values of 0 to 1000)
+  //to fit the range 0 to 50
+  const y = d3
+    .scaleLinear()
+    .domain([0, 1000])
+    .range([0, 500]);
+
   // join the data to rects
   const rects = svg.selectAll('rect').data(data);
 
   rects
     .attr('width', 50)
-    .attr('height', d => d.orders)
+    .attr('height', d => y(d.orders))
     .attr('fill', 'orange')
-    .attr('x', (d, i) => i * 70);
+    .attr('x', (d, i) => i * 70)
+    .attr('y', d => 500 - y(d.orders));
 
   // append the enter selection to the DOM
   rects
     .enter()
     .append('rect')
     .attr('width', 50)
-    .attr('height', d => d.orders)
+    .attr('height', d => y(d.orders))
     .attr('fill', 'orange')
-    .attr('x', (d, i) => i * 70);
+    .attr('x', (d, i) => i * 70)
+    .attr('y', d => 500 - y(d.orders));
 });
