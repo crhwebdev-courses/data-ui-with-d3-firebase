5678d773321775ec5d79748e693846f7251bbade
Carl Hain <crhain@gmail.com>
2019-09-05 13:15:29 -0500

    section5: index.js now pulls data from firebase firestore instead of from a json file

--- section5/index.js
+++ section5/index.js
@@ -23,68 +23,77 @@ const xAxisGroup = graph
   .attr('transform', `translate(0, ${graphHeight})`);
 const yAxisGroup = graph.append('g');
 
-//get data from json file
-d3.json('menu.json').then(data => {
-  //return mininum value for orders from dataset
-  // const min = d3.min(data, d => d.orders);
+//get data from firestore database
+db.collection('dishes')
+  .get()
+  .then(res => {
+    // let data = [];
+    // res.docs.forEach(doc => {
+    //   data.push(doc.data());
+    // });
 
-  //return maxinum value for orders from dataset
-  const max = d3.max(data, d => d.orders);
+    const data = res.docs.map(doc => doc.data());
 
-  //return an array with a mininum and maximum value
-  // const extent = d3.extent(data, d => d.orders);
+    //return mininum value for orders from dataset
+    // const min = d3.min(data, d => d.orders);
 
-  //create scale for y value -
-  //this will scale the values in the domain (data set values of 0 to 1000)
-  //to fit the range 0 to 50
-  const y = d3
-    .scaleLinear()
-    .domain([0, max])
-    .range([graphHeight, 0]);
+    //return maxinum value for orders from dataset
+    const max = d3.max(data, d => d.orders);
 
-  //create a band scale for x value -
-  // this will scale the values based on the number of items in the
-  // dataset
-  const x = d3
-    .scaleBand()
-    .domain(data.map(item => item.name))
-    .range([0, graphWidth])
-    .paddingInner(0.2)
-    .paddingOuter(0.2);
+    //return an array with a mininum and maximum value
+    // const extent = d3.extent(data, d => d.orders);
 
-  // join the data to rects
-  const rects = graph.selectAll('rect').data(data);
+    //create scale for y value -
+    //this will scale the values in the domain (data set values of 0 to 1000)
+    //to fit the range 0 to 50
+    const y = d3
+      .scaleLinear()
+      .domain([0, max])
+      .range([graphHeight, 0]);
 
-  rects
-    .attr('width', x.bandwidth)
-    .attr('height', d => graphHeight - y(d.orders))
-    .attr('fill', 'orange')
-    .attr('x', d => x(d.name))
-    .attr('y', d => y(d.orders));
+    //create a band scale for x value -
+    // this will scale the values based on the number of items in the
+    // dataset
+    const x = d3
+      .scaleBand()
+      .domain(data.map(item => item.name))
+      .range([0, graphWidth])
+      .paddingInner(0.2)
+      .paddingOuter(0.2);
 
-  // append the enter selection to the DOM
-  rects
-    .enter()
-    .append('rect')
-    .attr('width', x.bandwidth)
-    .attr('height', d => graphHeight - y(d.orders))
-    .attr('fill', 'orange')
-    .attr('x', d => x(d.name))
-    .attr('y', d => y(d.orders));
+    // join the data to rects
+    const rects = graph.selectAll('rect').data(data);
 
-  // create and call the axes using d3 functions
-  const xAxis = d3.axisBottom(x);
-  const yAxis = d3
-    .axisLeft(y)
-    .ticks(3)
-    .tickFormat(d => `${d} orders`);
+    rects
+      .attr('width', x.bandwidth)
+      .attr('height', d => graphHeight - y(d.orders))
+      .attr('fill', 'orange')
+      .attr('x', d => x(d.name))
+      .attr('y', d => y(d.orders));
 
-  xAxisGroup.call(xAxis);
-  yAxisGroup.call(yAxis);
+    // append the enter selection to the DOM
+    rects
+      .enter()
+      .append('rect')
+      .attr('width', x.bandwidth)
+      .attr('height', d => graphHeight - y(d.orders))
+      .attr('fill', 'orange')
+      .attr('x', d => x(d.name))
+      .attr('y', d => y(d.orders));
 
-  xAxisGroup
-    .selectAll('text')
-    .attr('transform', 'rotate(-40)')
-    .attr('text-anchor', 'end')
-    .attr('fill', 'orange');
-});
+    // create and call the axes using d3 functions
+    const xAxis = d3.axisBottom(x);
+    const yAxis = d3
+      .axisLeft(y)
+      .ticks(3)
+      .tickFormat(d => `${d} orders`);
+
+    xAxisGroup.call(xAxis);
+    yAxisGroup.call(yAxis);
+
+    xAxisGroup
+      .selectAll('text')
+      .attr('transform', 'rotate(-40)')
+      .attr('text-anchor', 'end')
+      .attr('fill', 'orange');
+  });
