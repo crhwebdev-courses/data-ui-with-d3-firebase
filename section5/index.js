c6c8432e38952766fe36dec56ee6e6aaf71b64a3
Carl Hain <crhain@gmail.com>
2019-09-07 13:01:03 -0500

    section5: update index.js to change graph when the firebase database has a change

--- section5/index.js
+++ section5/index.js
@@ -88,13 +88,39 @@ const update = data => {
   yAxisGroup.call(yAxis);
 };
 
-//get data from firestore database
-db.collection('dishes')
-  .get()
-  .then(res => {
-    //get data
-    const data = res.docs.map(doc => doc.data());
+let data = [];
 
-    //run update function
-    update(data);
+//get data from firestore database
+db.collection('dishes').onSnapshot(res => {
+  res.docChanges().forEach(change => {
+    const doc = { ...change.doc.data(), id: change.doc.id };
+    console.log(doc);
+    switch (change.type) {
+      case 'added':
+        data.push(doc);
+        break;
+      case 'modified':
+        const index = data.findIndex(item => item.id == doc.id);
+        data[index] = doc;
+        break;
+      case 'removed':
+        data = data.filter(item => item.id !== doc.id);
+        break;
+      default:
+        break;
+    }
   });
+
+  update(data);
+});
+
+// db.collection('dishes')
+//   .get()
+//   .then(res => {
+//     //get data
+//     const data = res.docs.map(doc => doc.data());
+
+//     //run update function
+//     update(data);
+
+//   });
