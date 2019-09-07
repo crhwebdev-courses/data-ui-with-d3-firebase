83c0229c5aa03e747ee9a7e804c0eeaf8b34617e
Carl Hain <crhain@gmail.com>
2019-09-07 13:21:08 -0500

    section5: refactor firebase change callback function to be simpler

--- section5/index.js
+++ section5/index.js
@@ -94,20 +94,28 @@ let data = [];
 db.collection('dishes').onSnapshot(res => {
   res.docChanges().forEach(change => {
     const doc = { ...change.doc.data(), id: change.doc.id };
-    switch (change.type) {
-      case 'added':
-        data.push(doc);
-        break;
-      case 'modified':
-        const index = data.findIndex(item => item.id == doc.id);
-        data[index] = doc;
-        break;
-      case 'removed':
-        data = data.filter(item => item.id !== doc.id);
-        break;
-      default:
-        break;
+    data = [...data, doc];
+
+    if (change.type === 'added' || change.type === 'modified') {
+      data = [...data, doc];
+    } else if (change.type === 'removed') {
+      data = data.filter(item => item.id !== doc.id);
     }
+
+    // switch (change.type) {
+    //   case 'added':
+    //     data = [...data, doc];
+    //     break;
+    //   case 'modified':
+    //     const index = data.findIndex(item => item.id == doc.id);
+    //     data[index] = doc;
+    //     break;
+    //   case 'removed':
+    //     data = data.filter(item => item.id !== doc.id);
+    //     break;
+    //   default:
+    //     break;
+    // }
   });
 
   update(data);
