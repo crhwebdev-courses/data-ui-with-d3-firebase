fbe90f11ba69324a150dbbfdf1b8a09f2b294e78
Carl Hain <crhain@gmail.com>
2019-11-21 10:12:22 -0600

    project3: wire up submit event listener for form that prints submited fields to console; fixed some typos

--- project3/index.js
+++ project3/index.js
@@ -1,3 +1,19 @@
 //setup modal window
 const modal = document.querySelector(".modal");
 M.Modal.init(modal);
+
+// get form elements from DOM
+const form = document.querySelector("form");
+const name = document.querySelector("#name");
+const parent = document.querySelector("#parent");
+const department = document.querySelector("#department");
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  console.log(name.value, parent.value, department.value);
+  // db.collection("employees").add({
+  //   name: name.value,
+  //   parent: parent.value,
+  //   department: department.value
+  // });
+});
